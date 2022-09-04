7e57e1334a15901daedb783987c73b21153d80c9
S0571740 <philip.blankenburg@student.htw-berlin.de>
2022-09-04 11:27:24 +0200

    add spring

---
+++ src/main/java/ALP/KBEProduct/KbeProductApplication.java
@@ -0,0 +1,13 @@
+package ALP.KBEProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbeProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ALP/KBEProduct/KbeProductApplicationTests.java
@@ -0,0 +1,13 @@
+package ALP.KBEProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
