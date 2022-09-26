75ca4bc8ce9ee7dd192a68340943718ccfe0f8bf
S0571740 <philip.blankenburg@student.htw-berlin.de>
2022-09-26 15:59:21 +0200

    add methods for getting all products/a single product

--- src/main/java/ALP/KBEProduct/ProductBuilder.java
+++ src/main/java/ALP/KBEProduct/ProductBuilder.java
@@ -1,21 +1,17 @@
 package ALP.KBEProduct;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import ALP.RabbitMessage;
 import ALP.KBEProduct.Model.Component;
 import ALP.KBEProduct.Model.Product;
-import ALP.KBEProduct.RabbitMQ.RabbitMQSender;
 
 @Service
 public class ProductBuilder {
 
-    @Autowired
-    RabbitMQSender productSender;
-    
     private HashMap<String, Product> productMap = new HashMap<>();
 
     public void handle(String productName, Component component){
@@ -24,8 +20,14 @@ public class ProductBuilder {
             product = new Product(productName);
         }
         product.addComponent(component);
-        System.out.println(product.toString());
         productMap.put(product.getName(), product);
-        productSender.send(new RabbitMessage("product", product));
+    }
+
+    public Product getProduct(String name){
+        return productMap.get(name);
+    }
+
+    public List<Product> getProducts(){
+        return productMap.values().stream().collect(Collectors.toList());
     }
 }
